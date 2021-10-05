6ed49280b00e74a4018caceb159a63ab0a97af94
Brooks Palin <brookspalin710@gmail.com>
2021-10-05 11:56:57 -0600

    work on game objects

--- src/scene.rs
+++ src/scene.rs
@@ -1,11 +1,31 @@
 use crate::math::Transform;
 use crate::draw::{Drawable};
+use crate::buffer::Buffer;
+use std::path::Component;
+use std::collections::HashMap;
 
 struct Scene {
-
+    gameobjects : HashMap<String, GameObject>
 }
 
-struct Object {
-    drawable: Box<Drawable>
+struct GameObject {
+    name : String,
 }
+
+impl GameObject {
+    fn new(name : &str) -> GameObject {
+        GameObject {
+            name: String::from(name)
+        }
+    }
+}
+
+struct GameComp<T : GameBehavior> {
+    behavior : T
+}
+
+trait GameBehavior {
+    fn update(&mut self);
+}
+
 
